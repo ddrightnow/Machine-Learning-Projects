5f4b652bd1b6cce7289eef31c68e4e7268cdd379
Dare Oluwajuyitan <ddrightnow@gmail.com>
2021-08-29 20:18:32 +0700

    pypi file package

---
+++ AWS Udacity Machine Learning/dd_distributions/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dd_distributions',
+      version='1.0',
+      description='Statistical distributions',
+      packages=['dd_distributions'],
+      author = 'Oludare Oluwajuyitan',
+      author_email = 'ddrightnow@yahoo.com',
+      zip_safe=False)
