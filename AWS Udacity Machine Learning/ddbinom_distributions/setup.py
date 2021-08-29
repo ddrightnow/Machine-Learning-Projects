c52551a7c7ab835c9fb094af375e909f2c38e427
Dare Oluwajuyitan <ddrightnow@gmail.com>
2021-08-29 21:39:37 +0700

    packages all uploaded to pypi

--- AWS Udacity Machine Learning/ddbinom_distributions/setup.py
+++ AWS Udacity Machine Learning/ddbinom_distributions/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='dd_distributions',
+setup(name='ddbinom_distributions',
       version='1.0',
       description='Statistical distributions',
-      packages=['dd_distributions'],
+      packages=['ddbinom_distributions'],
       author = 'Oludare Oluwajuyitan',
       author_email = 'ddrightnow@yahoo.com',
       zip_safe=False)
