bc3e446449c91c90eb4747a4c4e7e2b57522ff11
Dare Oluwajuyitan <ddrightnow@gmail.com>
2022-03-23 15:23:35 +0700

    l

--- Complete-Python-3-Bootcamp-master/Python testing build/MyMainPackage/SubPackage/mysubscript.py
+++
@@ -1,2 +0,0 @@
-def sub_report():
-	print("Hey I'm a function inside musubscript")
--- Complete-Python-3-Bootcamp-master/Python testing build/cap.py
+++
@@ -1,8 +0,0 @@
-def cap_text(text):
-    '''
-    Input a string
-    Output the capitalized string
-
-    '''
-
-    return text.title()
