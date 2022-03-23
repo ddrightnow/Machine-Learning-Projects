774d16940b3155745be7c8b211a27e88fe6cf45c
Dare Oluwajuyitan <ddrightnow@gmail.com>
2022-03-23 15:14:51 +0700

    update

--- AWS & Udacity Machine Learning/Binomial Class/4a_binomial_package/distributions/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Gaussiandistribution import Gaussian
-from .Binomialdistribution import Binomial
-
-# TODO: import the Binomial class from the Binomialdistribution module
--- AWS & Udacity Machine Learning/ddbinom_distributions/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='ddbinom_distributions',
-      version='1.0',
-      description='Statistical distributions',
-      packages=['ddbinom_distributions'],
-      author = 'Oludare Oluwajuyitan',
-      author_email = 'ddrightnow@yahoo.com',
-      zip_safe=False)
